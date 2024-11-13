fc5fa75ee6287c9b744a2e5e74c5ad7b1769064c
Meno96 <danielemeno96@gmail.com>
2024-11-13 18:35:44 +0100

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,28 @@
+import discord
+from bot_logic import gen_pass
+
+# la variabile intents contiene i permessi al bot
+intents = discord.Intents.default()
+# abilita il permesso a leggere i contenuti dei messaggi
+intents.message_content = True
+# crea un bot e passa gli indents
+client = discord.Client(intents=intents)
+
+@client.event
+async def on_ready():
+    print(f'Abbiamo fatto l\'accesso come {client.user}')
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+    if message.content.startswith('$ciao'):
+        await message.channel.send("Ciao!")
+    elif message.content.startswith('$arrivederci'):
+        await message.channel.send("\U0001f642")
+    elif message.content.startswith('$pass'):
+        await message.channel.send(gen_pass(10))
+    else:
+        await message.channel.send(message.content)
+
+client.run("INSERT_TOKEN")
