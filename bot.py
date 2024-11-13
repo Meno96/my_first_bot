a8379101464cfbfcbf1a98ee8b793d922aae9449
Meno96 <danielemeno96@gmail.com>
2024-11-13 19:17:17 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,28 +1,40 @@
 import discord
+from discord.ext import commands
 from bot_logic import gen_pass
+import random
 
 # la variabile intents contiene i permessi al bot
 intents = discord.Intents.default()
 # abilita il permesso a leggere i contenuti dei messaggi
 intents.message_content = True
 # crea un bot e passa gli indents
-client = discord.Client(intents=intents)
+bot = commands.Bot(command_prefix="$", intents=intents)
 
-@client.event
+@bot.event
 async def on_ready():
-    print(f'Abbiamo fatto l\'accesso come {client.user}')
+    print(f'Abbiamo fatto l\'accesso come {bot.user}')
 
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
-    if message.content.startswith('$ciao'):
-        await message.channel.send("Ciao!")
-    elif message.content.startswith('$arrivederci'):
-        await message.channel.send("\U0001f642")
-    elif message.content.startswith('$pass'):
-        await message.channel.send(gen_pass(10))
-    else:
-        await message.channel.send(message.content)
+@bot.command()
+async def ciao(ctx):
+    await ctx.send("Ciao!")
 
-client.run("INSERT_TOKEN")
+@bot.command()
+async def pasw(ctx):
+    await ctx.send(gen_pass(10))
+
+@bot.command()
+async def arrivederci(ctx):
+    await ctx.send("Arrivederci!")
+
+@bot.command()
+async def repeat(ctx, times: int, content='repeating...'):
+    """Repeats a message multiple times."""
+    for i in range(times):
+        await ctx.send(content)
+
+@bot.command(description='For when you wanna settle the score some other way')
+async def choose(ctx, *choices: str):
+    """Chooses between multiple choices."""
+    await ctx.send(random.choice(choices))
+
+bot.run("INSERT_TOKEN")
